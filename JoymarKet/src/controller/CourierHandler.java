95325af556f747316f74d849bf17480486e35655
AdityaEkaPratama <145786783+AdityaEkaPratama@users.noreply.github.com>
2025-12-18 21:52:45 +0700

    comment + review
    Added methods to retrieve courier and delivery data.

--- JoymarKet/src/controller/CourierHandler.java
+++ JoymarKet/src/controller/CourierHandler.java
@@ -5,16 +5,20 @@ import java.util.List;
 import model.Courier;
 import model.Delivery;
 
+// Controller that handles courier-related data access
 public class CourierHandler {
 	
+    // Get all deliveries assigned to a specific courier
 	public static List<Delivery> getCourierDeliveries(String idCourier) {
 		return Delivery.getCourierDeliveries(idCourier);
 	}
 	
+    // Get courier data based on courier ID
 	public static Courier getCourier(String idCourier) {
 	    return Courier.getCourier(idCourier);
 	}
 	
+    // Retrieve all available couriers from the database
 	public static List<Courier> getAllCouriers() {
 		return Courier.getAllCouriers();
 	}
