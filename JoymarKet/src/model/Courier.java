cd11b4cd896a2740a8c4108c684c99ee590998f1
AdityaEkaPratama <145786783+AdityaEkaPratama@users.noreply.github.com>
2025-12-18 22:55:54 +0700

    comment + review

--- JoymarKet/src/model/Courier.java
+++ JoymarKet/src/model/Courier.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import database.CourierDA;
 
+// Courier model class that extends User
 public class Courier extends User {
 
 	private String vehicleType;
 	private String vehiclePlate;
 	
+    // Constructor to initialize courier data
 	public Courier(String idUser, String fullName, String email, String password, String phone, String address,
 			String role, String vehicleType, String vehiclePlate) {
 		super(idUser, fullName, email, password, phone, address, role);
@@ -16,27 +18,33 @@ public class Courier extends User {
 		this.vehiclePlate = vehiclePlate;
 	}
 
+    // Get vehicle type
 	public String getVehicleType() {
 		return vehicleType;
 	}
 
+    // Set vehicle type
 	public void setVehicleType(String vehicleType) {
 		this.vehicleType = vehicleType;
 	}
 
+    // Get vehicle plate number
 	public String getVehiclePlate() {
 		return vehiclePlate;
 	}
 
+    // Set vehicle plate number
 	public void setVehiclePlate(String vehiclePlate) {
 		this.vehiclePlate = vehiclePlate;
 	}
 	
+    // Get courier data by courier ID
 	public static Courier getCourier(String idCourier) {
 		CourierDA courierDA = new CourierDA();
 		return courierDA.getCourier(idCourier);
 	}
 	
+    // Get all couriers from database
 	public static List<Courier> getAllCouriers() {
 	    CourierDA courierDA = new CourierDA();
 	    return courierDA.getAllCouriers();
