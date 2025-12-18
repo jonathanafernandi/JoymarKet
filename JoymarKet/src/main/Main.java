cf3e65163e427267d00e561fa76666aab3664976
AdityaEkaPratama <145786783+AdityaEkaPratama@users.noreply.github.com>
2025-12-18 22:52:50 +0700

    comment + review

--- JoymarKet/src/main/Main.java
+++ JoymarKet/src/main/Main.java
@@ -4,22 +4,27 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import view.UserWindow;
 
+// Main entry point of the application
 public class Main extends Application {
 
+    // Launch JavaFX application
 	public static void main(String[] args) {
 		launch(args);
-
 	}
 
+    // Start method called when application runs
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+
+        // Set application window title
 		primaryStage.setTitle("JoymarKet - Grocery Marketplace");
 		primaryStage.setResizable(false);
 		
+        // Show login window first
 		UserWindow.showLoginWindow(primaryStage);
 		
+        // Display the main stage
 		primaryStage.show();
-		
 	}
 
 }
