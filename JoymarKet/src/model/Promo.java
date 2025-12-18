3b0664f085d235844b2874acdee10f5bd38b1058
AdityaEkaPratama <145786783+AdityaEkaPratama@users.noreply.github.com>
2025-12-18 23:03:14 +0700

    comment + review

--- JoymarKet/src/model/Promo.java
+++ JoymarKet/src/model/Promo.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import database.PromoDA;
 
+// Promo model class that represents discount promotion data
 public class Promo {
 
 	private String idPromo;
@@ -11,6 +12,7 @@ public class Promo {
 	private String headline;
 	private double discountPercentage;
 	
+    // Constructor to initialize promo data
 	public Promo(String idPromo, String code, String headline, double discountPercentage) {
 		this.idPromo = idPromo;
 		this.code = code;
@@ -18,43 +20,53 @@ public class Promo {
 		this.discountPercentage = discountPercentage;
 	}
 
+    // Get promo ID
 	public String getIdPromo() {
 		return idPromo;
 	}
 
+    // Set promo ID
 	public void setIdPromo(String idPromo) {
 		this.idPromo = idPromo;
 	}
 
+    // Get promo code
 	public String getCode() {
 		return code;
 	}
 
+    // Set promo code
 	public void setCode(String code) {
 		this.code = code;
 	}
 
+    // Get promo headline
 	public String getHeadline() {
 		return headline;
 	}
 
+    // Set promo headline
 	public void setHeadline(String headline) {
 		this.headline = headline;
 	}
 
+    // Get discount percentage
 	public double getDiscountPercentage() {
 		return discountPercentage;
 	}
 
+    // Set discount percentage
 	public void setDiscountPercentage(double discountPercentage) {
 		this.discountPercentage = discountPercentage;
 	}
 	
+    // Get promo data using promo code
 	public static Promo getPromo(String code) {
 		PromoDA promoDA = new PromoDA();
 		return promoDA.getPromoByCode(code);
 	}
 	
+    // Get all promos from database
 	public static List<Promo> getAllPromos() {
 	    PromoDA promoDA = new PromoDA();
 	    return promoDA.getAllPromos();
